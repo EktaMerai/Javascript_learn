cfd0dc942d781d27e5898fce8697875a3824e286
EktaMerai <ektamerai83@gmail.com>
2022-02-09 19:00:41 +0530

    Changes in js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("Hello Ekta!!!");
+const laptops = ["dell","apple","asua"];
+/* using for of */
+for(let laptop of laptops){
+    console.log(laptop);
+}
+console.log("===========");
+/* using foreach */
+laptops.forEach(function(laptop){
+    console.log(laptop);
+})
+console.log("===========");
+/* using for in */
+for(let index in laptops){
+    console.log(laptops[index]);
+}
+console.log("===========");
+
+/* for syntax c and c++ way*/
+for(let index=0;index<laptops.length;index++ ){
+     console.log(laptops[index]);
+}
